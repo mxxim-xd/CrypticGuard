3d79bb11e0bd07620cefc7415f08c4abace2a994
Devilem-xD <maximbellerwn@gmail.com>
2024-07-31 23:16:51 +0200

    Removed paths

--- doc_crypt.sh
+++ doc_crypt.sh
@@ -5,12 +5,3 @@ if [ $1 == "decrypt" ]; then
 fi
 
 python3 doc_crypt.py $1
-
-# THIS SCRIPT RUNS AS PART OF THE crypt_routine.service with links in 
-# /etc/systemd/system/halt.target.wants
-# /etc/systemd/system/reboot.target.wants
-# /etc/systemd/system/shutdown.target.wants
-# /home/maxim/.config/systemd/user/default.target.wants
-# SERVICES ARE STORED IN /etc/systemd/system/ AS:
-# /etc/systemd/system/crypt_routine.service
-# /home/maxim/.config/systemd/user/crypt_boot_routine.service
