0a0af1e1c078c951241e5040988be3780dbcd36e
mxxim-xd <maximbellerwn@gmail.com>
2025-03-17 20:26:00 +0100

    Generating random AES key instead of password-derived key

--- src/sym_key_create.py
+++ src/sym_key_create.py
@@ -1,8 +1,6 @@
-from Crypto.Random import get_random_bytes
-from Crypto.Protocol.KDF import PBKDF2
+import os
 
-salt = get_random_bytes(32)
-key = PBKDF2("something", salt, dkLen=32)
+key = os.urandom(32)
 
 # Export key
 with open("key.bin", "wb") as f:
